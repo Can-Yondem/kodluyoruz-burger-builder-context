5ab4a01ffe43877acd06e2c99dd4e03b1ebd3eb1
Onur Can Yöndem <can.yondem.22@gmail.com>
2021-06-18 20:45:11 +0300

    bugs fixed

--- src/index.js
+++ src/index.js
@@ -7,13 +7,11 @@ import HamburgerContextProvider from "./context/HamburgerContext";
 
 ReactDOM.render(
   <React.StrictMode>
-      {/* Uygulamayı context provider ile sarmalayınız */}
+    <HamburgerContextProvider>
       <App />
+    </HamburgerContextProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
